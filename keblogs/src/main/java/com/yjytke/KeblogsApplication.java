57b052d0c6f591a637d2df61aefd654b1b58a890
yanyuting <yt_yan@outlook.com>
2018-09-21 15:14:54 +0800

    for package war

--- keblogs/src/main/java/com/yjytke/KeblogsApplication.java
+++ keblogs/src/main/java/com/yjytke/KeblogsApplication.java
@@ -3,13 +3,19 @@ package com.yjytke;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.yjytke.dao")
 @EnableCaching
-public class KeblogsApplication {
-
+public class KeblogsApplication extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder application) {
+        return application.sources(KeblogsApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(KeblogsApplication.class, args);
 	}
